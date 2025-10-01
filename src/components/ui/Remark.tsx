11f4b54748feaeb71106a2d87ddc84d510f647e9
BrunZo <ziger.bruno@gmail.com>
2025-10-01 02:18:49 -0300

    fix: add space to Remark

--- src/components/ui/Remark.tsx
+++ src/components/ui/Remark.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Lightbulb } from 'lucide-react';
 
-export default function Remark({ children }) {
+export default function Remark({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex items-center gap-2 bg-blue-100 p-4">
+    <div className="flex items-center rounded-md gap-2 bg-blue-100 p-4 mt-4">
       <div>
         <Lightbulb />
       </div>
